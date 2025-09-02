c29518fee76dc151acb42b0dc2b8a9c36c83f075
bryan1993-HA <thoury.bryan1993@gmail.com>
2025-09-02 02:15:48 +0200

    Update const.py

--- custom_components/domovra/const.py
+++ custom_components/domovra/const.py
@@ -1,4 +1,14 @@
 DOMAIN = "domovra"
 PLATFORMS = ["sensor"]
+
+# Options / conf
 CONF_BASE_URL = "base_url"
 DEFAULT_SCAN_SECS = 30
+
+# Device registry
+MANUFACTURER = "Domovra"
+MODEL = "Home Assistant Add-on"
+
+# Identifiant stable pour regrouper toutes les entités sous UN appareil
+# (si tu prévois plusieurs instances, utilise plutôt (DOMAIN, entry.entry_id))
+DEVICE_IDENTIFIER = (DOMAIN, "core")
