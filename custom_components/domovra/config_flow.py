aed846e9a7cc1fd9db973fcf3df9c8a5cdd8bd36
bryan1993-HA <thoury.bryan1993@gmail.com>
2025-09-02 03:20:38 +0200

    Update config_flow.py

--- custom_components/domovra/config_flow.py
+++ custom_components/domovra/config_flow.py
@@ -2,7 +2,7 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from .const import DOMAIN, CONF_BASE_URL
+from .const import DOMAIN, CONF_BASE_URL, DEFAULT_SCAN_SECS
 
 class DomovraConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -12,9 +12,12 @@ class DomovraConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             base_url = str(user_input[CONF_BASE_URL]).rstrip("/")
             await self.async_set_unique_id(base_url)
             self._abort_if_unique_id_configured()
-            return self.async_create_entry(title="Domovra", data={CONF_BASE_URL: base_url})
+            return self.async_create_entry(
+                title="Domovra",
+                data={CONF_BASE_URL: base_url},
+                options={"scan_interval": DEFAULT_SCAN_SECS},
+            )
 
-        # 👉 API directe de l'add-on (port exposé)
         schema = vol.Schema({
             vol.Required(CONF_BASE_URL, default="http://127.0.0.1:8099"): str
         })
@@ -24,13 +27,16 @@ class DomovraConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def async_get_options_flow(self, config_entry):
         return DomovraOptionsFlow(config_entry)
 
+
 class DomovraOptionsFlow(config_entries.OptionsFlow):
     def __init__(self, entry):
         self.entry = entry
 
     async def async_step_init(self, user_input=None):
-        scan = self.entry.options.get("scan_interval", 30)
-        schema = vol.Schema({vol.Required("scan_interval", default=scan): int})
+        scan = self.entry.options.get("scan_interval", DEFAULT_SCAN_SECS)
+        schema = vol.Schema({
+            vol.Required("scan_interval", default=scan): int
+        })
         if user_input:
             return self.async_create_entry(title="", data=user_input)
         return self.async_show_form(step_id="init", data_schema=schema)
